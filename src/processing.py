1a9cc5348a86b2147ebe71f36f8c9bd4a03056ee
ekaterina besshtannikova <dolmatova3010@yandex.ru>
2024-06-14 16:48:02 +0700

    создание функции для сортировки списка по ключу

--- src/processing.py
+++ src/processing.py
@@ -11,3 +11,10 @@ def filter_by_state(list_of_dictionaries: List[Dict], state: str = "EXECUTED") -
     return returned_list
 
 
+def sort_by_date(date_list: List[Dict], reverse: bool = True) -> List[Dict]:
+    """
+    Функция для сортировки словарей по убыванию.
+    """
+    sort_date = []
+    sort_date = sorted(date_list, key = lambda dates: dates['date'], reverse=True)
+    return sort_date
