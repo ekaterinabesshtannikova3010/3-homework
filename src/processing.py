c6b4a49e469283c5bac2cf540f3328fa925d7015
ekaterina besshtannikova <dolmatova3010@yandex.ru>
2024-06-15 11:47:18 +0700

    homework 10_1 done

--- src/processing.py
+++ src/processing.py
@@ -6,13 +6,14 @@ def filter_by_state(list_of_dictionaries: List[Dict], state: str = "EXECUTED") -
     Функция для поиска словарей с конкреным значением ключа.
     """
     returned_list = []
-    for i in list_of_dictionaries:
-        if i["state"] == state:
-            returned_list.append(i)
+    for lis in list_of_dictionaries:
+        if lis["state"] == state:
+            returned_list.append(lis)
     return returned_list
 
 
 def sort_by_date(date_list: List[Dict], reverse: bool = True) -> List[Dict]:
     """Функция для сортировки словарей по убыванию."""
-    sort_date = sorted(date_list, key=lambda dates: dates['date'], reverse=True)
-    return sort_date
+    sorted_list = sorted(date_list, key=lambda date_entry: date_entry['date'], reverse=reverse)
+    return sorted_list
+
